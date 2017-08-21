8c0b1588f456389135aa9be92be38ab5efb4a53d
“Aaron <barfootaaron@yahoo.com>
2017-08-21 16:00:31 -0500

    kill nickelback completed

---
+++ kill-nickelback/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kill_nickelback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, string)> goodSongs = new List<(string, string)>();
+
+            HashSet<(string, string)> allSongs = new HashSet<(string, string)>();
+
+            allSongs.Add(("Gravity", "John Mayer"));
+            allSongs.Add(("Photograph", "Nickelback"));
+            allSongs.Add(("Far Away", "Nickelback"));
+            allSongs.Add(("Ants Marching", "Dave Matthews"));
+            allSongs.Add(("Someday", "Nickelback"));
+            allSongs.Add(("How you remind me", "Nickelback"));
+            allSongs.Add(("What's Goin On", "Marvin Gaye"));
+
+            foreach(var song in allSongs)
+            {
+                if (song.Item2 != "Nickelback")
+                {
+                    goodSongs.Add(song);
+                }
+            }
+
+            foreach(var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Item1} by {song.Item2}");
+            }
+        }
+    }
+}
